8ca3c2e7285105d9fa032cb61fe4de0d5ed5a8f8
Maria Musante <mmusante@frba.utn.edu.ar>
2025-08-27 18:00:11 -0300

    agrego teoria

--- CirBnb/PracticoCIRBNB/domain.js
+++ CirBnb/PracticoCIRBNB/domain.js
@@ -1,103 +1,101 @@
 //Ponele q esto es un enum
 export const Categoria = Object.freeze({
-    Hotel: "Hotel",
-    Departamento: "Departamento",
-    Cabania: "Cabaña",
-    Apart: "Apart"
+  Hotel: "Hotel",
+  Departamento: "Departamento",
+  Cabania: "Cabaña",
+  Apart: "Apart",
 });
 
-export class Alojamiento{
-    constructor(nombre, precioPorNoche, categoria){
-        this.nombre = nombre;
-        this.precioPorNoche = precioPorNoche;
-        this.categoria = categoria;
-    }
+export class Alojamiento {
+  constructor(nombre, precioPorNoche, categoria) {
+    this.nombre = nombre;
+    this.precioPorNoche = precioPorNoche;
+    this.categoria = categoria;
+  }
 
-    //esto es un metodo
-    getDescription(){
-        return `${this.nombre} (${this.categoria}) - $${this.precioPorNoche} por noche`;
-    }
+  //esto es un metodo
+  getDescription() {
+    return `${this.nombre} (${this.categoria}) - $${this.precioPorNoche} por noche`;
+  }
 }
 
-export class Reserva{
-    constructor(alojamiento, diaInicio, diaFin){
-
-        if(!diaInicio instanceof Date  || !diaFin instanceof Date){
-            throw new Error("Dia inicio y fin deben ser instancia de Date");
-        }
-
-        if(diaInicio >= diaFin){
-            throw new Error("Dia inicio debe ser previa al dia de fin");
-        }
-        
-        this.alojamiento = alojamiento;
-        this.diaInicio = diaInicio;
-        this.diaFin = diaFin;
-        this.descuentos = [];
-
+export class Reserva {
+  constructor(alojamiento, diaInicio, diaFin) {
+    if ((!diaInicio) instanceof Date || (!diaFin) instanceof Date) {
+      throw new Error("Dia inicio y fin deben ser instancia de Date");
     }
 
-    cantidadDeNoches() {
-        const msPorDia = 1000 * 60 * 60 * 24; 
-        return Math.ceil((this.diaFin - this.diaInicio) / msPorDia); //Hago esto porq me devuelve la diferencia en milisegundos
+    if (diaInicio >= diaFin) {
+      throw new Error("Dia inicio debe ser previa al dia de fin");
     }
 
-    precioBase(){
-        return this.cantidadDeNoches() * this.alojamiento.precioPorNoche;
+    this.alojamiento = alojamiento;
+    this.diaInicio = diaInicio;
+    this.diaFin = diaFin;
+    this.descuentos = [];
+  }
+
+  cantidadDeNoches() {
+    const msPorDia = 1000 * 60 * 60 * 24;
+    return Math.ceil((this.diaFin - this.diaInicio) / msPorDia); //Hago esto porq me devuelve la diferencia en milisegundos
+  }
+
+  precioBase() {
+    return this.cantidadDeNoches() * this.alojamiento.precioPorNoche;
+  }
+
+  agregarDescuento(descuento) {
+    this.descuentos.push(descuento);
+  }
+
+  precioFinal() {
+    let base = this.precioBase();
+    let totalDescontado = 0;
+
+    for (const desc of this.descuentos) {
+      totalDescontado += desc.valorDescontado(base, this.cantidadDeNoches());
     }
 
-    agregarDescuento(descuento){
-        this.descuentos.push(descuento);
-    }
-
-    precioFinal() {
-        let base = this.precioBase();
-        let totalDescontado = 0;
-
-        for(const desc of this.descuentos) {
-            totalDescontado += desc.valorDescontado(base , this.cantidadDeNoches())
-        }
-
-        return Math.max(0, base - totalDescontado);
-    }
+    return Math.max(0, base - totalDescontado);
+  }
 }
 
 export class DescuentoFijo {
-    constructor(valor){
-        this.valor = valor;
-    }
+  constructor(valor) {
+    this.valor = valor;
+  }
 
-    valorDescontado(precioBase , cantidad){
-        return this.valor;
-    }
+  valorDescontado(precioBase, cantidad) {
+    return this.valor;
+  }
 }
 
 export class DescuentoPorcentaje {
-    constructor(porcentaje) {
-        this.porcentaje = porcentaje;
-    }
+  constructor(porcentaje) {
+    this.porcentaje = porcentaje;
+  }
 
-    valorDescontado(precioBase , cantidad) {
-        return precioBase * (this.porcentaje / 100);
-    }
+  valorDescontado(precioBase, cantidad) {
+    return precioBase * (this.porcentaje / 100);
+  }
 }
 
 export class DescuentoPorNoches {
-    constructor(cantidadNoches , porcentaje){
-        this.cantidadNoches = cantidadNoches;
-        this.porcentaje = porcentaje;
+  constructor(cantidadNoches, porcentaje) {
+    this.cantidadNoches = cantidadNoches;
+    this.porcentaje = porcentaje;
+  }
+
+  valorDescontado(precioBase, cantidad) {
+    const vecesRepetidos = Math.floor(cantidad / this.cantidadNoches);
+    let valorDescontado = 0;
+
+    if (vecesRepetidos >= 1) {
+      valorDescontado += precioBase * (this.porcentaje / 100) * vecesRepetidos;
     }
 
-    valorDescontado(precioBase , cantidad) {
-        const vecesRepetidos = Math.floor(cantidad / this.cantidadNoches);
-        let valorDescontado = 0;
-
-        if (vecesRepetidos >= 1){
-            valorDescontado += precioBase * (this.porcentaje / 100) * vecesRepetidos;
-        }
-
-        return valorDescontado;
-    }
+    return valorDescontado;
+  }
 }
 
 // module.exports = {
--- CirBnb/PracticoCIRBNB/funciones.js
+++ CirBnb/PracticoCIRBNB/funciones.js
@@ -1,36 +1,33 @@
-export function aumentarPrecioDiario(alojamientos , aumento) {
-    alojamientos.forEach(a => {
-        a.precioPorNoche = a.precioPorNoche + aumento;
-    });
+export function aumentarPrecioDiario(alojamientos, aumento) {
+  alojamientos.forEach((a) => {
+    a.precioPorNoche = a.precioPorNoche + aumento;
+  });
 }
 
-export function alojamientoMasCaro(alojamientos){
-    const listaDePrecios = alojamientos.map(a => {
-        return a.precioPorNoche;
-    });
-    const precioMaximo = Math.max(...listaDePrecios); //... pasa todos los elementos de la lista
+export function alojamientoMasCaro(alojamientos) {
+  const listaDePrecios = alojamientos.map((a) => {
+    return a.precioPorNoche;
+  });
+  const precioMaximo = Math.max(...listaDePrecios); //... pasa todos los elementos de la lista
 
-    const alojamiento = alojamientos.find(a => {
-        return a.precioPorNoche == precioMaximo;
-    })
+  const alojamiento = alojamientos.find((a) => {
+    return a.precioPorNoche == precioMaximo;
+  });
 
-
-    return alojamiento;
+  return alojamiento;
 }
 
-export function filtrarPorPrecio(alojamientos, precioMaximo){
-
-    return alojamientos.filter(a => {
-        return a.precioPorNoche <= precioMaximo;
-    })
+export function filtrarPorPrecio(alojamientos, precioMaximo) {
+  return alojamientos.filter((a) => {
+    return a.precioPorNoche <= precioMaximo;
+  });
 }
 
-export function obtenerTotalReservas(reservas){
-
-    //el reduce es como un fold
-    return reservas.reduce((total, reserva) => {
-        return total + reserva.precioFinal();
-    }, 0);
+export function obtenerTotalReservas(reservas) {
+  //el reduce es como un fold
+  return reservas.reduce((total, reserva) => {
+    return total + reserva.precioFinal();
+  }, 0);
 }
 
 // module.exports = {
